95ab352dad89d77a947ba42dad6e3ad364bced5d
Haowei Li <haoweilee666@gmail.com>
2022-03-10 18:07:19 -0500

    debugged flashlight class and now it is fully functional

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.workoutapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.workoutapp.flashlight.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    private flashlight fl;
+    private Button btnOF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fl = new flashlight(this);
+        btnOF = (Button) findViewById(R.id.btnOnOff);
+
+        btnOF.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+
+                fl.turnOn();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/workoutapp/flashlight.java
+++ app/src/main/java/com/example/workoutapp/flashlight.java
@@ -8,6 +8,7 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.text.Editable;
+import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
@@ -21,6 +22,16 @@ public class flashlight {
         return onOff;
     }
     @RequiresApi(api = Build.VERSION_CODES.M)
+    public void turnOn(){
+        onOff = true;
+        update();
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void turnOff(){
+        onOff = false;
+        update();
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void update() {
         if(onOff){
             try {
@@ -30,16 +41,25 @@ public class flashlight {
             }
         }else{
             try {
+
                 mCameraManager.setTorchMode(mCameraId, false);
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
         }
     }
+    public boolean getIsFlashAvailable(){
+        return isFlashAvailable;
+    }
     public flashlight(Context context){
         this.context = context;
         this.onOff = false;
         this.mCameraManager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
         this.isFlashAvailable = context.getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
     }
