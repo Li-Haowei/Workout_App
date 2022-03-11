fe7424ce3b16a8cd954533b8bdb2d00cbf145369
Wenxuan Yan <yanwx@bu.edu>
2022-03-11 10:06:11 -0500

    one step

--- app/src/main/java/com/example/workoutapp/MainActivity.java
+++ app/src/main/java/com/example/workoutapp/MainActivity.java
@@ -7,9 +7,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.workoutapp.flashlight.*;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private flashlight fl;
@@ -28,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //fl.blinkFlash(10);  //This enables a button for blinking 10 seconds
 
+            }
+        });
+
+        final ListView list = findViewById(R.id.list);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Easy");
+        arrayList.add("Medium");
+        arrayList.add("Hard");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,                   android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickedItem=(String) list.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this,clickedItem,Toast.LENGTH_LONG).show();
             }
         });
     }
